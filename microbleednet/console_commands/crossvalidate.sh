d343ba319de3d2a049feb6119f32aafb83177c4e
Gouri Shanker <gouriswag1@gmail.com>
2025-01-16 22:38:03 +0530

    Addded sample crossvalidate bash script

--- microbleednet/console_commands/crossvalidate.sh
+++ microbleednet/console_commands/crossvalidate.sh
@@ -2,5 +2,5 @@
 
 # Sample crossvalidate command, update the arguments before use
 export MICROBLEEDNET_PRETRAINED_MODEL_PATH='/path-to-pretrained-model-checkpoints'
-microbleednet cross_validate -i /home/deepika/gouriworkshere/CerebralMicrobleed/Data/CMB_Preprocessed_FRST_sum_2_3 -l /home/deepika/gouriworkshere/CerebralMicrobleed/Data/CMB_Preprocessed_FRST_sum_2_3 -o /home/deepika/gouriworkshere/CerebralMicrobleed/forked_microbleednet_outputs -int=True -v=True
+microbleednet cross_validate -i /directory-containing-files-preprocessed-using-microbleednet-preprocess-function -l /this-argument-is-not-actually-used-but-make-sure-to-use-a-dummy-directory -o /output-directory -int=True -v=True
 
