cfba8f302cac694b0fab421425c2c4df93f85676
vsundaresan <vaanu.us@gmail.com>
2025-04-07 14:22:28 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -12,7 +12,6 @@ setup(
     install_requires=install_requires,
     scripts=[
         'microbleednet/console_commands/microbleednet',
-        'microbleednet/console_commands/prepare_microbleednet_data',
 	],
 	packages=find_packages(),
     include_package_data=True,
